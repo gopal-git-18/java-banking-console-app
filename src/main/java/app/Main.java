5465fa718c4cd0c46c2d449270de6eae8c1d36e2
gopal-git-18 <gopalshete057@gmail.com>
2025-12-30 18:18:46 +0530

    first commit of the project after defining extact feture in main file

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        System.out.println("Welcome to console bank");
+        while(running){
+            System.out.println("""
+                1) Open Account
+                2) Deposit
+                3) Withdraw
+                4) Transfer
+                5) Account Statement
+                6) List Accounts
+                7) Search Accounts by Customer Name
+                0) Exit
+            """);
+            System.out.println("CHOOSE: ");
+            String choice = scanner.nextLine().trim();
+            System.out.println("Choice: " + choice);
+
+
+        }
+    }
+}
